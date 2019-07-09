9e68b3974a5598e103db8df56ea2799ef8f0ea61
kirshanth <krishanthkirsh@gmail.com>
2019-07-09 18:13:19 +0530

    HTTP helper

---
+++ ABCRestaurant/ABCRestaurant.Web/AppSettingsConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABCRestaurant.Web
+{
+    public class AppSettingsConfiguration
+    {
+        public string apiBasicUri { get; set; }
+    }
+}
--- ABCRestaurant/ABCRestaurant.Web/Controllers/MenuController.cs
+++ ABCRestaurant/ABCRestaurant.Web/Controllers/MenuController.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ABCRestaurant.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ABCRestaurant.Web.Controllers
 {
     public class MenuController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            List<User> claimTerms = await HttpHelper.Get<List<User>>("/api/User/");
             return View();
         }
     }
